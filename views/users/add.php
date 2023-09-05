41ad97102973d6b75339b4f83bcbb714d5ec812a
Rasha a <racha.atassi@gmail.com>
2023-09-05 14:02:28 +0200

    another corrections

---
+++ views/users/add.php
@@ -0,0 +1,14 @@
+<h2>Entrez vos informations</h2>
+
+
+<form action="index.php?controller=user&action=sendAddUser" method="POST">
+        <label for="nom">Votre NOM</label>
+        <input type="text" id="nom" placeholder="ex: LUMIER"><br>
+        <label for="prenom">Votre Prénom</label>
+        <input type="text" id="prenom" placeholder="ex: Marie"><br>
+        <label for="email">Votre email</label>
+        <input type="email" id="email" placeholder="ex: marie@gmail.com"><br>
+        <label for="motdepasse">Votre mot de passe</label>
+        <input type="password" id="motdepasse"><br>
+        <button type="submit">Submit</button>
+    </form>
