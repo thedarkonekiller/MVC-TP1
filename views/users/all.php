ee6ee0930df5db755b2a8fcb74990e24d9fb7e39
Rasha a <racha.atassi@gmail.com>
2023-09-05 13:28:08 +0200

    singup - sign in - all (users)

---
+++ views/users/all.php
@@ -0,0 +1,45 @@
+<?php 
+    $pageTitle = "Notre Utilisateurs";
+?>
+
+<h1>Utilisateurs de notre site&nbsp;:</h1>
+
+<table border=1>
+        <thead>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Email</th>
+            <th>Mot de Pass</th>
+            <th>Modifier</th>
+            <th>Supprimer</th>
+        </thead>
+        <tbody>
+            <?php foreach($users as $user){ ?>
+                <tr>
+                    <td><?= $user["lastName"]; ?></td>
+                    <td><?= $user["firstName"]; ?></td>
+                    <td><?= $user["email"]; ?></td>
+                    <td><?= $user["password"]; ?></td>
+                    <td>
+                        <form method="POST" action="index.php?controller=user&action=.................(showUpdateForm)">
+                            <input type="hidden" value="<?= $user["id"]; ?>" name="updateID">
+                            <input type="submit" value="📝">
+                        </form>
+                    </td>
+                    <td>
+                        <form method="POST" action="index.php?controller=user&action=.........delete">
+                            <input type="hidden" value="<?= $user["id"]; ?>" name="deleteID">
+                            <input type="submit" value="🗑️">
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+    <!-- 
+        Le chemin du lien ci dessous n'est plus valide (puisque l'on se
+        base sur l'index) : à vous de le mettre à jour en prenant en compte
+        le controlleur et l'action adéquate.
+    -->
+    <p><a href="index.php?controller=book&action=showAddForm" title="Ajouter un utilisateur">Ajouter un utilisateur</a></p>
---
+++ views/users/signin.php
@@ -0,0 +1,8 @@
+
+<h2>se connecter</h2>
+<form action="" method="post">
+<label for="email">Votre email</label>
+        <input type="email" id="email" placeholder="ex: marie@gmail.com"><br>
+        <label for="motdepasse">Votre mot de passe</label>
+        <input type="password" id="motdepasse"><br>
+</form>
